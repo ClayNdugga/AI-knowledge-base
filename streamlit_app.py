e7f0292e5288512f8536de422a4084b7668b5471
clayndugga <clayndugga@gmail.com>
2024-02-27 06:03:22 -0500

    update read me

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit.components.v1 import components  # Import components correctly
 import requests
 
 #######################################################################################################
@@ -52,7 +53,7 @@ def page1():
     st.title("📖 Abstract GPT")
 
     # Text input fields
-    st.markdown("Abstract GPT uses **Knowledge Base Embedding's** stored in a **vector DB** to augment LLM queries with relevant research paper abstracts to create more accurate responses.")
+    st.markdown("Abstract GPT uses **[Retrival Augmented Generation (RAG)](https://blogs.nvidia.com/blog/what-is-retrieval-augmented-generation/)** to improve **LLM** performance when generating research paper abstracts (summaries)")
     st.markdown(
             "## How to use\n"
             "1. Enter the title for paper 📄\n"
@@ -121,12 +122,61 @@ def page2():
         """)
 
 def page3():
-    st.markdown("DIAGRM")
+    st.markdown("""
+    ## Overview
+
+   This project demonstrates a serverless architecture for generating professional paper abstracts using **Large Language Models (LLMs)**. It employs a **Retrieval-Augmented Generation (RAG)** approach, integrating LLM prompt engineering with queries to a vector database for contextual relevance and accuracy. This solution showcases practices and tools that are crucial for modern, scalable applications. \\
+    \\
+    At the heart of this project lies the **RAG** approach, which significantly enhances the capability of LLMs by incorporating a retrieval step into the generation process. This methodology involves querying a vector database to find relevant information that contextually enriches the model's output. In the context of this application, when the user requests an abstract, RAG gets the 3 most relevant exisitng abstracts from the vector store, then sends them to the LLM with the intital prompt to increase the quality of the result.
+
+    ## Application Across Domains
+
+    The principles and architecture demonstrated in this project can be translated to a wide range of applications beyond paper abstract generation. For instance, it can be adapted for:
+
+    - **Question Answering Systems**: Enhancing the accuracy and relevance of answers by retrieving similar questions or related information.
+    - **Content Recommendation**: Augmenting recommendation engines by incorporating retrieval-based context to personalize content.
+    - **Language Understanding Tasks**: Improving natural language processing applications by providing relevant background information for better context understanding.
+
+    ## Project Architecture
+    """)
+    # components.html("""   
+    # <figure align="center">
+    # <img src="https://github.com/ClayNdugga/AI-knowledge-base/blob/main/assets/architecture.png?raw=true" alt="architecture"/>
+    # <figcaption style="text-align: center;">
+    #     <i>Serverless Architecture</i>
+    # </figcaption>
+    # </figure>
+    # """)
+    st.image("https://github.com/ClayNdugga/AI-knowledge-base/blob/main/assets/architecture.png?raw=true")
+    st.caption("Serverless Architecture")
+
+    st.markdown("""
+    The diagram outlines the flow of data and interactions between the user, web application, and various AWS services, including the utilization of FAISS for vector search capabilities.
+
+    1. **Web Application**: A streamlit UI hosted on the public cloud serves as the front-end to take paper titles and findings
+    2. **Amazon API Gateway**: Manages and routes incoming API requests to the lambda function.
+    3. **AWS Lambda**: Handles incoming API request: \\
+    **3.1.** Process request to extract paper title and findings submitted by the user\\
+    **3.2.** Search S3 bucket using FAISS to find relevant paper abstracts \\
+    **3.3.** Makes call to OPEN AI API to genrate abstract, call is augmented with relevant abstracts from 3.2 for reference \\
+    **3.4.** Return the generated paper title and abstract
+
+    4. **Vector Store Updates**: The S3 vector store is updated offline and uses FAISS. FAISS is a highly efficient library for similarity search and clustering of dense vectors that scales to billions of vectors. The raw text data containing paper titles and abstracts is stored in an offline DB.
+
+
+    ### Technologies & Frameworks
+
+    - **AWS**: For serverless compute, storage, and API management.
+    - **Vector Database**: Utilizes vector databases for efficient similarity searches, supporting the RAG approach.
+    - **LangChain**: For integrating LLM operations (LLMOps)
+    - **Streamlit**: For creating interactive web UI
+
+    """)
 
 pages = {
     "Use Abstract GPT": page1,
     "Examples": page2,
-    "How it work's": page3
+    "How it works": page3
 }
 
 
@@ -134,7 +184,7 @@ def main():
     with st.sidebar:
 
         st.markdown("# Navigation")
-        page = st.radio("", ["Use Abstract GPT", "How it work's","Examples"])
+        page = st.radio("", ["Use Abstract GPT", "How it works","Examples"])
 
         # st.markdown("---")
         # st.markdown(
@@ -158,7 +208,7 @@ def main():
         page1()
     elif page == "Examples":
         page2()
-    elif page == "How it work's":
+    elif page == "How it works":
         page3()
 
 
